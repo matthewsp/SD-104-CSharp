b7fe176191abbd7e326a88e778400bdac7a5ff4a
Matthew <matthew-s-p@att.net>
2018-04-20 19:59:55 -0500

    array lab

---
+++ Lab Reverse Array/Lab Reverse Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab_Reverse_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that takes a user input n times
+            // Write it in an array
+            // Loop through it, and display the array backwards
+            int input;
+            Console.WriteLine("Please enter how many numbers you want in your array.");
+            while (!Int32.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("That's not a number. Enter a number.");
+            }
+            int[] UserArray = new int[input];
+            for (int x = 0; x < UserArray.Length; x++)
+            {
+                Console.WriteLine("Enter the value for number " + (x + 1));
+                while (!Int32.TryParse(Console.ReadLine(), out input))
+                {
+                    Console.WriteLine("That's not a number. Enter a number.");
+                }
+                UserArray[x] = input;
+            }
+            for(int x = UserArray.Length - 1; x >= 0; x--)
+            {
+                if (x != 0)
+                {
+                    Console.Write(UserArray[x] + ", ");
+                }
+                else
+                    Console.WriteLine(UserArray[x]);
+            }
+        }
+    }
+}
---
+++ ages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays
+{
+    class ages
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = { 80, 18, 65, 75 };
+            for (int i =0; i < ages.Length; i++)
+            {
+                if( ages[i] >= 65)
+                {
+                Console.WriteLine(i + 1);
+                }
+
+            }
+            Console.Read();
+        }
+    }
+}
